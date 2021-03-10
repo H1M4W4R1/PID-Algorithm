c0ccb412215c610900a81390c5c91b186d155dea
Patryk Pastuszka <nov11157@gmail.com>
2021-03-10 10:12:36 +0100

    Update PID.cpp

--- PID.cpp
+++ PID.cpp
@@ -14,15 +14,15 @@ PID *PID::setPwmMaxValue(int32_t value) {
     return this;
 }
 
-uint32_t PID::calculatePWM(int32_t readValue, int32_t deltaTime_ms) {
+int32_t PID::calculatePWM(int32_t readValue, int32_t deltaTime_ms) {
     // Calculate value
-    uint32_t error = targetValue - readValue + errOffset;
+    int32_t error = targetValue - readValue + errOffset;
 
     // Calculate PID value
-    uint32_t pid_p = 0.01f * k_p * error; // P
+    int32_t pid_p = 0.01f * k_p * error; // P
     pid_i = 0.01f * pid_i + (k_i * error); // I
-    uint32_t pid_d = 0.01f * k_d * (((float)error - (float)last_error)/((float)deltaTime_ms/1000.0f)); // D
-    uint32_t pid_value = pid_p + pid_i + pid_d; // PID value
+    int32_t pid_d = 0.01f * k_d * (((float)error - (float)last_error)/((float)deltaTime_ms/1000.0f)); // D
+    int32_t pid_value = pid_p + pid_i + pid_d; // PID value
 
     // Limit PID values
     if(pid_value < 0) pid_value = 0;
@@ -33,13 +33,13 @@ uint32_t PID::calculatePWM(int32_t readValue, int32_t deltaTime_ms) {
     return pid_value;
 }
 
-PID::PID(uint32_t kp, uint32_t ki, uint32_t kd) {
+PID::PID(int32_t kp, int32_t ki, int32_t kd) {
     k_p = kp;
     k_d = kd;
     k_i = ki;
 }
 
-PID *PID::setErrorOffset(uint32_t offset) {
+PID *PID::setErrorOffset(int32_t offset) {
     errOffset = offset;
     return this;
 }
